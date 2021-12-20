bcc2dfbd69d7576d09de087fa388c53ea4affabf
Imamkoo <imammm.kooo@gmail.com>
2021-12-20 15:23:04 +0700

    Tugas 27 Javascript

---
+++ tugas27.js
@@ -0,0 +1,9 @@
+let a = "NO";
+let b = "rumah saya";
+let c = 30;
+let d = "A";
+let e = "Jalan pangeran Tirtayasa No";
+let f = 70;
+
+console.log(a + " " + b + " " + String(c) + " " + d);
+console.log(e + " " + String(f));
